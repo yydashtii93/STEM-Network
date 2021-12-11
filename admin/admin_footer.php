5486396dff4da4d71a45ba77f34c3f56be2815db
Yousef Mohammed <91865856+yydashtii93@users.noreply.github.com>
2021-12-10 18:47:47 -0800

    Add files via upload

---
+++ admin/admin_footer.php
@@ -0,0 +1,55 @@
+<footer>
+    <div class="no-print row no-gutters justify-content-between fs--1 mt-4 mb-3">
+        <div class="col-12 col-sm-auto text-center">
+            <p class="mb-0 text-600 opacity-85">&copy; STEM Project 2021</p>
+        </div>
+    </div>
+</footer>
+</div>
+
+
+</div>
+</main>
+
+<!-- ===============================================-->
+<!--    JavaScripts-->
+<!-- ===============================================-->
+
+<script src="../assets/js/jquery.min.js"></script>
+<script src="../assets/js/popper.min.js"></script>
+<script src="../assets/js/bootstrap.min.js"></script>
+<script src="../assets/lib/@fortawesome/all.min.js"></script>
+<script src="../assets/lib/stickyfilljs/stickyfill.min.js"></script>
+<script src="../assets/lib/sticky-kit/sticky-kit.min.js"></script>
+<script src="../assets/lib/is_js/is.min.js"></script>
+<script src="../assets/lib/lodash/lodash.min.js"></script>
+<script src="../assets/lib/perfect-scrollbar/perfect-scrollbar.js"></script>
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,500,600,700%7cPoppins:100,200,300,400,500,600,700,800,900&amp;display=swap" rel="stylesheet">
+<script src="../assets/lib/chart.js/Chart.min.js"></script>
+<script src="../assets/lib/datatables/js/jquery.dataTables.min.js"></script>
+<script src="../assets/lib/datatables-bs4/dataTables.bootstrap4.min.js"></script>
+<script src="../assets/lib/datatables.net-responsive/dataTables.responsive.js"></script>
+<script src="../assets/lib/datatables.net-responsive-bs4/responsive.bootstrap4.js"></script>
+<script src="../assets/lib/leaflet/leaflet.js"></script>
+<script src="../assets/lib/leaflet.markercluster/leaflet.markercluster.js"></script>
+<script src="../assets/lib/leaflet.tilelayer.colorfilter/leaflet-tilelayer-colorfilter.min.js"></script>
+<script src="../assets/js/theme.min.js"></script>
+<script src="../assets/lib/flatpickr/flatpickr.min.js"></script>
+<script src="../assets/lib/select2/select2.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js" integrity="sha512-2ImtlRlf2VVmiGZsjm9bEyhjGW4dU7B6TNwh/hx/iSByxNENtj3WVE6o/9Lj4TJeVXPi4bnOIMXFIJJAeufa0A==" crossorigin="anonymous"></script>
+<script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.5.2/js/dataTables.buttons.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.bootstrap4.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.print.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.colVis.min.js"></script>
+<script src="https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js"></script>
+<script src="https://cdn.datatables.net/responsive/2.2.3/js/responsive.bootstrap4.min.js"></script>
+
+</body>
+
+</html>
