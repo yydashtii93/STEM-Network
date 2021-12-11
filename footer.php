1b76bb27d6cb75e138b9be5895abd765b7253006
Yousef Mohammed <91865856+yydashtii93@users.noreply.github.com>
2021-12-10 19:20:45 -0800

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,11 @@
+</main>
+
+<!-- JAVASCRIPT LIBRARIES -->
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/theme.min.js"></script>
+<script src="assets/js/popper.min.js"></script>
+<script src="assets/lib/is_js/is.min.js"></script>
+<script src="assets/lib/@fortawesome/all.min.js"></script>
+</body>
+
+</html>
